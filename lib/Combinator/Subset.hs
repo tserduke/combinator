48fd5b5810adb56cd5228629326d794a5d294180
Taras Serduke <taras.serduke@gmail.com>
2016-09-26 17:24:55 +0300

    basic subset api

--- lib/Combinator/Subset.hs
+++ lib/Combinator/Subset.hs
@@ -1,5 +1,27 @@
 module Combinator.Subset where
 
+import Data.Hot
 
-subsets :: a
-subsets = undefined
+
+subsetCount :: Int -> Int -> Integer
+subsetCount = undefined
+
+
+subsetFromNum :: (Integral a) => Int -> Int -> Integer -> [a]
+subsetFromNum = undefined
+
+subsetToNum :: (Foldable t, Integral a) => Int -> Int -> t a -> Integer
+subsetToNum = undefined
+
+
+{-# INLINE subsets #-}
+subsets :: (Num a) => Int -> [Hot n a]
+subsets = subsetsFilter (const True)
+
+{-# INLINABLE subsetsFilter #-}
+subsetsFilter :: (Num a) => (a -> Bool) -> Int -> [Hot n a]
+subsetsFilter = undefined
+
+
+--subsetsSelect :: (Num a) => (a -> b) -> Int -> [Hot n b]
+--subsetsSelectFilter :: (Num a) => (a -> b) -> (a -> Bool) -> Int -> [Hot n b]
